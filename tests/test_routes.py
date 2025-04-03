f81296ca4f9b19dfeb2f3fda4c578985892182a6
vvakond <vvakond@gmail.com>
2025-04-03 05:29:54 -0400

    read account impl

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,16 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+         """It should Read an Account"""
+         account = self._create_accounts(1)[0]
+         response = self.client.get(f"{BASE_URL}/{account.id}")
+         self.assertEqual(response.status_code, status.HTTP_200_OK)
+         # Check data is correct
+         read_account = response.get_json()
+         self.assertEqual(read_account["id"], account.id)
+         self.assertEqual(read_account["name"], account.name)
+         self.assertEqual(read_account["email"], account.email)
+         self.assertEqual(read_account["address"], account.address)
+         self.assertEqual(read_account["phone_number"], account.phone_number)
+         self.assertEqual(read_account["date_joined"], str(account.date_joined))
